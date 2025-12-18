4dc7aca1e0b36b8527cf12a940d228d32f2836f1
KimEunHye <gpqjr00@naver.com>
2025-12-19 03:22:22 +0900

    Feat: Mock API 호출 함수 구현

---
+++ src/api/MockAxios.ts
@@ -0,0 +1,71 @@
+import MockInstance from './MockInstance';
+
+export const getMockPosts = async () => {
+  try {
+    const { data } = await MockInstance('/posts');
+    console.log('posts', data);
+    return data;
+  } catch (error) {
+    console.error('게시글 목록 로드 에러: ', error);
+  }
+};
+
+export const getCoffeeBrands = async () => {
+  try {
+    const { data } = await MockInstance('/top-coffee-brands');
+    console.log('coffee', data);
+    return data;
+  } catch (error) {
+    console.error('인기 커피 브랜드 로드 에러: ', error);
+  }
+};
+
+export const getSnackBrands = async () => {
+  try {
+    const { data } = await MockInstance('/popular-snack-brands');
+    console.log('snack', data);
+    return data;
+  } catch (error) {
+    console.error('인기 간식 브랜드 로드 에러: ', error);
+  }
+};
+
+export const getMoodTrend = async () => {
+  try {
+    const { data } = await MockInstance('/weekly-mood-trend');
+    console.log('mood', data);
+    return data;
+  } catch (error) {
+    console.error('주간 무드 트렌드 로드 에러: ', error);
+  }
+};
+
+export const getWorkoutTrend = async () => {
+  try {
+    const { data } = await MockInstance('/weekly-workout-trend');
+    console.log('workout', data);
+    return data;
+  } catch (error) {
+    console.error('주간 운동 트렌드 로드 에러: ', error);
+  }
+};
+
+export const getCoffeeConsumption = async () => {
+  try {
+    const { data } = await MockInstance('/coffee-consumption');
+    console.log('workout', data);
+    return data;
+  } catch (error) {
+    console.error('팀별 커피 소비/버그/생산성 로드 에러: ', error);
+  }
+};
+
+export const getSnackImpact = async () => {
+  try {
+    const { data } = await MockInstance('/snack-impact');
+    console.log('workout', data);
+    return data;
+  } catch (error) {
+    console.error('부서별 간식 영향 로드 에러: ', error);
+  }
+};
