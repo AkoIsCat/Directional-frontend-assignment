4dc7f75ae9241b377f97773412988f938d00779a
KimEunHye <gpqjr00@naver.com>
2025-12-19 03:26:58 +0900

    Feat: AuthWrapper 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,24 @@ import Analytics from './pages/Analytics.tsx';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Route, Routes, Navigate } from 'react-router';
+import AuthWrapper from './components/AuthWrapper.tsx';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<Navigate to="/posts" replace />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/create" element={<CreatePost />} />
-        <Route path="/posts/:id/edit" element={<EditPost />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/my-page" element={<MyPage />} />
-      </Routes>
+      <AuthWrapper>
+        <Routes>
+          <Route path="/" element={<Navigate to="/posts" replace />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/create" element={<CreatePost />} />
+          <Route path="/posts/:id/edit" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+          <Route path="/my-page" element={<MyPage />} />
+        </Routes>
+      </AuthWrapper>
     </QueryClientProvider>
   );
 }
