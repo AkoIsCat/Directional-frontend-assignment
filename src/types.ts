ce93fab36669c7d875d6382b46982409e6813cc5
KimEunHye <gpqjr00@naver.com>
2025-12-19 03:23:51 +0900

    Feat: CreatePostType, UpdatePostType 정의

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,18 @@ import React from 'react';
 export interface Children {
   children: React.ReactNode;
 }
+
+export type CreatePostType = {
+  title: string;
+  contents: string;
+  category: 'NOTICE' | 'QNA' | 'FREE';
+  tags: string;
+};
+
+export type UpdatePostType = {
+  id: number;
+  title: string;
+  contents: string;
+  category: 'NOTICE' | 'QNA' | 'FREE';
+  tags: string[];
+};
