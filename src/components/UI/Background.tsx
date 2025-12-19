7cc7b084b43c643c61b6ec9eaa184660bacdbe26
KimEunHye <gpqjr00@naver.com>
2025-12-20 01:09:02 +0900

    Feat: Background 컴포넌트 작성

--- src/components/UI/Background.tsx
+++ src/components/UI/Background.tsx
@@ -1,7 +1,9 @@
 import type { Children } from '../../types';
 
 const Background = ({ children }: Children) => {
-  return <div className="w-screen h-screen bg-white flex">{children}</div>;
+  return (
+    <div className="w-screen bg-white flex overflow-x-hidden">{children}</div>
+  );
 };
 
 export default Background;
