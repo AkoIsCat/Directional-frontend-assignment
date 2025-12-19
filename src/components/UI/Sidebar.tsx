c22518a066c6c566d26e1973014cffd940a596ff
KimEunHye <gpqjr00@naver.com>
2025-12-20 01:09:17 +0900

    Feat: 왼쪽 게시판/그래프 선택 사이드바 작성

--- src/components/UI/Sidebar.tsx
+++ src/components/UI/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-sidebar bg-sidebar flex flex-col gap-12 items-center pt-10">
+    <div className="w-sidebar min-h-screen bg-sidebar flex flex-col gap-12 items-center pt-10 shrink-0">
       <div className="cursor-pointer w-iconHover h-iconHover hover:bg-iconHover flex justify-center items-center rounded-hover">
         <Board onClick={() => navigate('/posts')} />
       </div>
